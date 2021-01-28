b247effd56873e39ad4e9ba5b56742ce0b6b8880
Alexander Fabisch <afabisch@googlemail.com>
2021-01-29 00:11:17 +0100

    Test gpx loader

---
+++ slither/test/test_gpx_loader.py
@@ -0,0 +1,19 @@
+import os
+from slither.gpx_loader import GpxLoader
+from nose.tools import assert_equal, assert_in
+
+
+def test_load_tracked_activity():
+    with open(os.path.join("test_data", "running.gpx")) as f:
+        tcx_content = f.read()
+    loader = GpxLoader(tcx_content)
+
+    activity = loader.load()
+    paths = activity.get_path()
+
+    assert_in("timestamps", paths)
+    assert_in("coords", paths)
+    assert_in("altitudes", paths)
+    assert_in("heartrates", paths)
+    assert_in("velocities", paths)
+    assert_equal(len(paths["timestamps"]), 3032)
