c9002ca02714c7dea9c37555a575e8cece8a904a
Alexander Fabisch <afabisch@googlemail.com>
2021-01-28 22:30:35 +0100

    Accept ending .xml for TCX

--- slither/loader.py
+++ slither/loader.py
@@ -7,7 +7,7 @@ class Loader:
 
     def get_loader(self, file_content):
         ending = self.filename.split(".")[-1]
-        if ending.lower() == "tcx":
+        if ending.lower() in ["tcx", "xml"]:
             return TcxLoader(file_content)
         else:
             raise ValueError("Cannot handle file format '%s'" % ending)
