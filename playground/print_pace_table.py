6cd28556c6bf2622c7fd3c77bbc8278dc452788d
Alexander Fabisch <afabisch@googlemail.com>
2021-02-21 18:05:40 +0100

    Print pace table

---
+++ playground/print_pace_table.py
@@ -0,0 +1,22 @@
+import sys
+from rich.table import Table
+from rich.console import Console
+from slither.io.tcx_loader import TcxLoader
+from slither.analysis import get_paces
+from slither.ui_text import d
+
+
+filename = sys.argv[-1]
+with open(filename, "r") as f:
+    loader = TcxLoader(f.read())
+
+a = loader.load()
+pace_table = get_paces(a)
+
+table = Table(title="Paces")
+table.add_column("Distance", justify="right")
+table.add_column("Pace (Time per km)", justify="right")
+for distance, pace in pace_table:
+    table.add_row(d.display_distance(distance), d.display_time(pace))
+console = Console()
+console.print(table)
