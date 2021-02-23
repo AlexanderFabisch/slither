04ccdba9c2198c35bd0ddbad8d314142e1185e54
Alexander Fabisch <afabisch@googlemail.com>
2021-02-24 00:29:28 +0100

    Update query

--- playground/print_personal_bests.py
+++ playground/print_personal_bests.py
@@ -1,6 +1,6 @@
-import numpy as np
 from rich.table import Table
 from rich.console import Console
+from rich import box
 from slither.service import Service
 from slither.ui_text import d
 from slither import domain_model
@@ -9,25 +9,35 @@ from sqlalchemy import func
 
 s = Service()
 
-# TODO filter invalid
-records = s.database.session.query(domain_model.Record, func.rank().over(
+subquery = s.database.session.query(domain_model.Record, func.rank().over(
     order_by=domain_model.Record.time.asc(),
     partition_by=(domain_model.Record.sport, domain_model.Record.distance)
-).label("rank")).all()
+).label("rank")).subquery()
+query = s.database.session.query(
+    subquery).filter(subquery.c.rank <= 30).filter(
+    subquery.c.valid).filter(subquery.c.time != float("inf"))
 
-table = Table(title="Records")
+table = Table(title="Records", box=box.MINIMAL_HEAVY_HEAD)
 table.add_column("Sport")
 table.add_column("Distance", justify="right")
 table.add_column("Rank", justify="right")
 table.add_column("Time", justify="right")
-for record, rank in records:  # print date
-    if rank >= 20 or not record.valid or np.isinf(record.time):
-        continue
+table.add_column("Date")
+table.add_column("Activity distance")
+table.add_column("Activity time")
+for _, sport, distance, time, valid, activity_id, rank in query.all():  # print date
+    assert valid
+    activity = s.database.session.query(
+        domain_model.Activity).filter(
+        domain_model.Activity.id == activity_id).first()
     table.add_row(
-        d.display_sport(record.sport),
-        d.display_distance(record.distance),
+        d.display_sport(sport),
+        d.display_distance(distance),
         str(rank),
-        d.display_time(record.time))
+        d.display_time(time),
+        d.display_date(activity.start_time),
+        d.display_distance(activity.distance),
+        d.display_time(activity.time))
 
 console = Console()
 console.print(table)
