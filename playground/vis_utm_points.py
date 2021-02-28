212eac5a2b258319a3c968bf6945efe7609a8f9b
Alexander Fabisch <afabisch@googlemail.com>
2021-02-28 16:46:58 +0100

    Create 3D visualization

---
+++ playground/vis_utm_points.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import utm  # https://github.com/Turbo87/utm
+from pytransform3d import visualizer
+import open3d as o3d
+from slither.service import Service
+from slither.ui_text import convert_m_to_km
+
+
+def all_trackpoints(lat_range, lon_range):
+    s = Service()
+    df = pd.read_sql_table("trackpoints", s.database.engine, index_col="id", columns=["latitude", "longitude", "altitude"])
+    df.latitude = np.rad2deg(df.latitude)
+    df.longitude = np.rad2deg(df.longitude)
+    df = df[df.latitude >= lat_range[0]]
+    df = df[df.latitude <= lat_range[1]]
+    df = df[df.longitude >= lon_range[0]]
+    df = df[df.longitude <= lon_range[1]]
+    df.dropna(inplace=True)
+    return df.latitude.to_numpy(), df.longitude.to_numpy(), df.altitude.to_numpy()
+
+
+alt_scale = 10.0
+lat_range = (38.0, 41.0)#(52.5, 53.5)
+lon_range = (1.0, 4.0)#(8.5, 10.5)
+lats, lons, alts = all_trackpoints(lat_range, lon_range)
+eastings, northings, zone_number, zone_letter = utm.from_latlon(lats, lons, force_zone_number=31, force_zone_letter="T")
+eastings = convert_m_to_km(eastings)
+northings = convert_m_to_km(northings)
+alts = convert_m_to_km(alts)
+alts *= alt_scale
+
+fig = visualizer.figure()
+pc = o3d.geometry.PointCloud()
+points = np.hstack((eastings[:, np.newaxis], northings[:, np.newaxis], alts[:, np.newaxis]))
+mean = np.mean(points, axis=0)
+points -= mean[np.newaxis]
+pc.points = o3d.utility.Vector3dVector(points)
+fig.add_geometry(pc)
+fig.view_init()
+fig.show()
