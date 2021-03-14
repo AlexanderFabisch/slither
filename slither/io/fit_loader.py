6361ec0665ceb9f47dc040bbe04f782e26de00a6
Alexander Fabisch <afabisch@googlemail.com>
2021-03-14 01:46:18 +0100

    Add FitLoader

---
+++ slither/io/fit_loader.py
@@ -0,0 +1,67 @@
+import time
+import numpy as np
+from slither.core.geodetic import compute_velocities
+from slither.domain_model import Activity
+from fitparse import FitFile
+
+
+class FitLoader:
+    """Loads Flexible and Interoperable Data Transfer (FIT) files."""
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self):
+        fitfile = FitFile(self.filename)
+        records = list(fitfile.get_messages("record"))
+        n_trackpoints = len(records)
+
+        sport_messages = list(fitfile.get_messages("sport"))
+        assert len(sport_messages) == 1
+        sport = sport_messages[0].get_value("sport")
+
+        activity_messages = list(fitfile.get_messages("activity"))
+        assert len(activity_messages) == 1
+        start_time = activity_messages[0].get_value("local_timestamp")
+        total_timer_time = activity_messages[0].get_value("total_timer_time")
+
+        session_messages = list(fitfile.get_messages("session"))
+        assert len(session_messages) == 1
+        distance = session_messages[0].get_value("total_distance")
+        calories = session_messages[0].get_value("total_calories")
+        heartrate = session_messages[0].get_value("avg_heart_rate")
+
+        activity = Activity(
+            sport=sport, start_time=start_time, time=total_timer_time,
+            distance=distance, calories=calories, heartrate=heartrate,
+            filetype="fit", has_path=n_trackpoints > 0)
+
+        if activity.has_path:
+            path = {
+                "timestamps": np.empty(n_trackpoints),
+                "coords": np.empty((n_trackpoints, 2)),
+                "altitudes": np.empty(n_trackpoints),
+                "heartrates": np.empty(n_trackpoints)
+            }
+
+            for i, record in enumerate(records):
+                record = record.get_values()
+                path["timestamps"][i] = time.mktime(record["timestamp"].timetuple())
+                if "position_lat" in record and "position_long" in record:
+                    path["coords"][i] = record["position_lat"], record["position_long"]
+                else:
+                    path["coords"][i] = np.nan
+                if "altitude" in record:
+                    path["altitudes"][i] = record["altitude"]
+                else:
+                    path["altitudes"][i] = np.nan
+                if "heart_rate" in record:
+                    path["heartrates"][i] = record["heart_rate"]
+                else:
+                    path["heartrates"][i] = np.nan
+
+            path["velocities"], _ = compute_velocities(
+                path["timestamps"], path["coords"])
+
+            activity.set_path(**path)
+
+        return activity
