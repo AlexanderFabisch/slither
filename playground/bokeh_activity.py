d0dbc4d9227b32809c7be5eedbf2c5a87cd9bdb6
Alexander Fabisch <afabisch@googlemail.com>
2021-03-21 19:39:22 +0100

    Enable hovering

--- playground/bokeh_activity.py
+++ playground/bokeh_activity.py
@@ -1,15 +1,19 @@
 # Demo with bokeh (https://docs.bokeh.org/en/latest/)
+# Requirements: bokeh, pandas
 # Start with:
 # bokeh serve --show playground/bokeh_activity.py
 import numpy as np
+import pandas as pd
 from slither.io.tcx_loader import TcxLoader
 from slither.core.config import config
-from slither.core.analysis import filtered_heartrates, filter_median_average
 from slither.core.unit_conversions import convert_mps_to_kmph, minutes_from_start
 
 from bokeh import plotting as plt
 from bokeh.io import curdoc
-from bokeh.layouts import column, row
+from bokeh.layouts import column
+from bokeh.models.sources import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.models import CustomJSHover
 
 
 filename = "/home/afabisch/.slither/data/running_20190825_112213.tcx"  # TODO
@@ -21,35 +25,63 @@ assert activity.has_path
 
 
 path = activity.get_path()
-time_in_min = minutes_from_start(path["timestamps"])
-velocities = convert_mps_to_kmph(
-    filter_median_average(path["velocities"], config["plot"]["filter_width"]))
+path["latitudes"] = path["coords"][:, 0]
+path["longitudes"] = path["coords"][:, 1]
+del path["coords"]
+df = pd.DataFrame(path)
+df["timestamps"] = minutes_from_start(df["timestamps"])
+time_in_min = df["timestamps"]
+df["velocities"] = df["velocities"].rolling(config["plot"]["filter_width"]).median()
+df["velocities"] = df["velocities"].rolling(config["plot"]["filter_width"]).mean()
+df["velocities"] = convert_mps_to_kmph(df["velocities"])
+df["heartrates"] = df["heartrates"].rolling(config["plot"]["filter_width"]).median()
+df["zeros"] = np.zeros(len(df))
+source = ColumnDataSource(df)
 
-heartrates = filtered_heartrates(path, config["plot"]["filter_width"])
-altitudes = path["altitudes"]
-
-tools = "wheel_zoom,xbox_select,reset"
+tooltips = [
+    ("Time", "@timestamps{custom}"),
+    ("Velocity", "@velocities km/h"),
+    ("Heartrate", "@heartrates bpm"),
+    ("Altitude", "@altitudes m"),
+]
+minutes_custom = CustomJSHover(code="""
+    var minutes = Math.floor(special_vars.x)
+    var seconds = Math.floor((special_vars.x - minutes) * 60.0)
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    return minutes + ":" + seconds + " minutes"
+""")
+hover = HoverTool(
+    tooltips=tooltips,
+    formatters={"@timestamps": minutes_custom},
+    mode="vline"
+)
+tools = [hover]
 
 plot_velocity = plt.figure(
     title="Velocity",
     sizing_mode="stretch_width", plot_height=150,
     tools=tools,
     x_axis_label="Time [min]", y_axis_label="Velocity [km/h]")
-plot_velocity.varea(x=time_in_min, y1=np.zeros_like(velocities), y2=velocities, color="blue", fill_alpha=0.3)
+plot_velocity.line(x="timestamps", y="velocities", color="blue", source=source)
+plot_velocity.varea(x="timestamps", y1="zeros", y2="velocities", color="blue", fill_alpha=0.3, source=source)
 
 plot_heartrate = plt.figure(
     title="Heartrate",
     sizing_mode="stretch_width", plot_height=150,
     tools=tools,
     x_axis_label="Time [min]", y_axis_label="Heartrate [bpm]")
-plot_heartrate.varea(x=time_in_min, y1=np.zeros_like(heartrates), y2=heartrates, color="red", fill_alpha=0.3)
+plot_heartrate.line(x="timestamps", y="heartrates", color="red", source=source)
+plot_heartrate.varea(x="timestamps", y1="zeros", y2="heartrates", color="red", fill_alpha=0.3, source=source)
 
 plot_altitude = plt.figure(
     title="Heartrate",
     sizing_mode="stretch_width", plot_height=150,
     tools=tools,
     x_axis_label="Time [min]", y_axis_label="Altitude [m]")
-plot_altitude.varea(x=time_in_min, y1=np.zeros_like(altitudes), y2=altitudes, color="green", fill_alpha=0.3)
+plot_altitude.line(x="timestamps", y="altitudes", color="green", source=source)
+plot_altitude.varea(x="timestamps", y1="zeros", y2="altitudes", color="green", fill_alpha=0.3, source=source)
 
 layout = column(plot_velocity, plot_heartrate, plot_altitude)
 curdoc().add_root(layout)
