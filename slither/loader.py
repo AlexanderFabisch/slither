bb31af3942449d15e50649c3c5a7441709090399
Alexander Fabisch <afabisch@googlemail.com>
2021-03-31 23:16:44 +0200

    Enable fit import

--- slither/loader.py
+++ slither/loader.py
@@ -10,12 +10,14 @@ class Loader:
     def __init__(self, filename):
         self.filename = filename
 
-    def get_loader(self, file_content):
+    def get_loader(self, file_content=None):
         ending = self.filename.split(".")[-1]
         if ending.lower() == "gpx":
             return GpxLoader(file_content)
         if ending.lower() in ["tcx", "xml"]:
             return TcxLoader(file_content)
+        if ending.lower() == "fit":
+            return FitLoader(self.filename)
         else:
             raise ValueError("Cannot handle file format '%s'" % ending)
 
