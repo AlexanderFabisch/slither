0a89acf63cd40002c2a5a47f4077d3b9bfebd5fd
Alexander Fabisch <afabisch@googlemail.com>
2021-03-31 22:44:49 +0200

    Extract read_fit

--- slither/io/fit_loader.py
+++ slither/io/fit_loader.py
@@ -12,56 +12,79 @@ class FitLoader:
         self.filename = filename
 
     def load(self):
-        fitfile = FitFile(self.filename)
-        records = list(fitfile.get_messages("record"))
-        n_trackpoints = len(records)
-
-        sport_messages = list(fitfile.get_messages("sport"))
-        assert len(sport_messages) == 1
-        sport = sport_messages[0].get_value("sport")
-
-        activity_messages = list(fitfile.get_messages("activity"))
-        assert len(activity_messages) == 1
-        start_time = activity_messages[0].get_value("local_timestamp")
-        total_timer_time = activity_messages[0].get_value("total_timer_time")
-
-        session_messages = list(fitfile.get_messages("session"))
-        assert len(session_messages) == 1
-        distance = session_messages[0].get_value("total_distance")
-        calories = session_messages[0].get_value("total_calories")
-        heartrate = session_messages[0].get_value("avg_heart_rate")
-
-        activity = Activity(
-            sport=sport, start_time=start_time, time=total_timer_time,
-            distance=distance, calories=calories, heartrate=heartrate,
-            filetype="fit", has_path=n_trackpoints > 0)
-
+        metadata, path = read_fit(self.filename)
+        activity = Activity(**metadata)
         if activity.has_path:
-            path = {
-                "timestamps": np.empty(n_trackpoints),
-                "coords": np.empty((n_trackpoints, 2)),
-                "altitudes": np.empty(n_trackpoints),
-                "heartrates": np.empty(n_trackpoints)
-            }
-            for k in path.keys():
-                path[k][:] = np.nan
-
-            for i, record in enumerate(records):
-                record = record.get_values()
-                path["timestamps"][i] = time.mktime(record["timestamp"].timetuple())
-                if "position_lat" in record and "position_long" in record:
-                    path["coords"][i] = record["position_lat"], record["position_long"]
-                if "altitude" in record:
-                    path["altitudes"][i] = record["altitude"]
-                if "heart_rate" in record:
-                    path["heartrates"][i] = record["heart_rate"]
-
-            finite_coords = np.isfinite(path["coords"])
-            path["coords"][finite_coords] = semicircles_to_radians(
-                path["coords"][finite_coords])
-            path["velocities"], _ = compute_velocities(
-                path["timestamps"], path["coords"])
-
             activity.set_path(**path)
-
         return activity
+
+
+def read_fit(filename):
+    """Read Flexible and Interoperable Data Transfer file (FIT).
+
+    Parameters
+    ----------
+    filename : str
+        Filename
+
+    Returns
+    -------
+    metadata : dict
+        Activity metadata
+
+    path : dict
+        Trackpoint data
+    """
+    fitfile = FitFile(filename)
+    records = list(fitfile.get_messages("record"))
+    n_trackpoints = len(records)
+    sport_messages = list(fitfile.get_messages("sport"))
+    assert len(sport_messages) == 1
+    sport = sport_messages[0].get_value("sport")
+    activity_messages = list(fitfile.get_messages("activity"))
+    assert len(activity_messages) == 1
+    start_time = activity_messages[0].get_value("local_timestamp")
+    total_timer_time = activity_messages[0].get_value("total_timer_time")
+    session_messages = list(fitfile.get_messages("session"))
+    assert len(session_messages) == 1
+    distance = session_messages[0].get_value("total_distance")
+    calories = session_messages[0].get_value("total_calories")
+    heartrate = session_messages[0].get_value("avg_heart_rate")
+    metadata = {
+        "sport": sport,
+        "start_time": start_time,
+        "time": total_timer_time,
+        "distance": distance,
+        "calories": calories,
+        "heartrate": heartrate,
+        "filetype": "fit",
+        "has_path": n_trackpoints > 0
+    }
+    if metadata["has_path"]:
+        path = {
+            "timestamps": np.empty(n_trackpoints),
+            "coords": np.empty((n_trackpoints, 2)),
+            "altitudes": np.empty(n_trackpoints),
+            "heartrates": np.empty(n_trackpoints)
+        }
+        for k in path.keys():
+            path[k][:] = np.nan
+
+        for i, record in enumerate(records):
+            record = record.get_values()
+            path["timestamps"][i] = time.mktime(record["timestamp"].timetuple())
+            if "position_lat" in record and "position_long" in record:
+                path["coords"][i] = record["position_lat"], record["position_long"]
+            if "altitude" in record:
+                path["altitudes"][i] = record["altitude"]
+            if "heart_rate" in record:
+                path["heartrates"][i] = record["heart_rate"]
+
+        finite_coords = np.isfinite(path["coords"])
+        path["coords"][finite_coords] = semicircles_to_radians(
+            path["coords"][finite_coords])
+        path["velocities"], _ = compute_velocities(
+            path["timestamps"], path["coords"])
+    else:
+        path = None
+    return metadata, path
