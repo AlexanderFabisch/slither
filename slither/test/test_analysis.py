3bb1e0b9e6482bb5a5caf0196308ab56556e6af6
Alexander Fabisch <afabisch@googlemail.com>
2022-03-09 22:03:55 +0100

    Test analysis functions

---
+++ slither/test/test_analysis.py
@@ -0,0 +1,31 @@
+import numpy as np
+from slither.core.analysis import (
+    check_coords, interpolate_nan, filtered_heartrates, appropriate_partition)
+from numpy.testing import assert_almost_equal
+from nose.tools import assert_equal
+
+
+def test_check_coords():
+    coords = np.array([[0, 1], [np.nan, 2], [2, 3]])
+    checked_coords = check_coords(coords)
+    assert_almost_equal(checked_coords, np.array([[0, 1], [2, 3]]))
+
+
+def test_interpolate_nan():
+    a = np.array([0, 1, np.nan, 3, 4, 5])
+    b = interpolate_nan(a)
+    assert_almost_equal(b, [0, 1, 2, 3, 4, 5])
+
+
+def test_filtered_heartrates():
+    path = {"heartrates": np.array([100, 101, 100, 101, 104])}
+    hrs = filtered_heartrates(path, 3)
+    assert_almost_equal(hrs, [100, 100, 101, 101, 101])
+
+
+def test_appropriate_partition():
+    assert_equal(appropriate_partition(1000), 400)
+    assert_equal(appropriate_partition(10000), 1000)
+    assert_equal(appropriate_partition(30000), 2000)
+    assert_equal(appropriate_partition(50000), 5000)
+    assert_equal(appropriate_partition(200000), 10000)
