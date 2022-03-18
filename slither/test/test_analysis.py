4072265d5a7825da5660c87b37058fa03732b62a
Alexander Fabisch <afabisch@googlemail.com>
2022-03-18 22:58:35 +0100

    Test paces

--- slither/test/test_analysis.py
+++ slither/test/test_analysis.py
@@ -1,7 +1,8 @@
 import numpy as np
 from slither.core.analysis import (
     check_coords, interpolate_nan, filtered_heartrates, appropriate_partition,
-    elevation_summary)
+    elevation_summary, get_paces)
+from slither.loader import FitLoader
 from numpy.testing import assert_almost_equal
 from nose.tools import assert_equal
 
@@ -44,3 +45,24 @@ def test_elevation_summary():
     assert_equal(gain, 0.0)
     assert_equal(loss, 1.0)
     assert_equal(slope_in_percent, 0.0)
+
+
+def test_get_paces():
+    fit_loader = FitLoader(filename="test_data/running.fit")
+    activity = fit_loader.load()
+    path = activity.get_path()
+    paces = get_paces(path, activity.sport)
+    assert_equal(paces[0][0], 1000)
+    assert_equal(paces[1][0], 2000)
+    assert_equal(paces[2][0], 3000)
+    assert_equal(paces[3][0], 4000)
+    assert_equal(paces[4][0], 5000)
+    assert_equal(paces[5][0], 6000)
+    assert_equal(paces[6][0], 7000)
+    assert_equal(paces[0][1], 369)
+    assert_equal(paces[1][1], 353)
+    assert_equal(paces[2][1], 363)
+    assert_equal(paces[3][1], 388)
+    assert_equal(paces[4][1], 361)
+    assert_equal(paces[5][1], 384)
+    assert_equal(paces[6][1], 391)
