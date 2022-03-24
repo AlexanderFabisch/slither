06928b3f250d80b7232237739f88b8237d30916e
Alexander Fabisch <afabisch@googlemail.com>
2022-03-25 00:29:31 +0100

    Test empty map

--- slither/test/test_visualization.py
+++ slither/test/test_visualization.py
@@ -1,3 +1,4 @@
+import numpy as np
 from slither.loader import TcxLoader
 from slither.core.visualization import make_map
 from nose.tools import assert_equal, assert_in
@@ -8,6 +9,13 @@ def test_make_map():
         loader = TcxLoader(f.read())
     activity = loader.load()
     map = make_map(activity.get_path())
+    d = map.to_dict()
+    assert_equal(d["name"], "Map")
+    assert_in("openstreetmap", d["children"])
+
+
+def test_make_empty_map():
+    map = make_map({"coords": np.zeros((0, 2))})
     d = map.to_dict()
     assert_equal(d["name"], "Map")
     assert_in("openstreetmap", d["children"])
